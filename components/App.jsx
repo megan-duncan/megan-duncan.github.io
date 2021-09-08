e8379649616ba4a9ec52f9a5a178031676eaddc8
megan-duncan <megan.j.duncan@gmail.com>
2021-09-08 20:59:01 +1200

    handle routing via useState

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Nav from "./Nav.jsx";
+import Home from "./Home.jsx";
+import BlogIndex from "./BlogIndex.jsx";
 
 const App = () => {
+  const [view, setView] = useState('home')
    return (
-   <Nav/>
+     <>
+     <Nav setView={setView}/>
+     {view === 'home' &&
+        <Home/>
+     }
+     {view === 'blog-index' &&
+      <BlogIndex setView={setView}/>
+     }
+    </>
     )
 };
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-const Nav = () => {
+import Home from "./Home.jsx";
+
+const Nav = ({ setView }) => {
+  const handleClick = (string) => {
+    setView(string)
+  }
    return (
   <div className="header">
-      <div className="container">
-        <nav className="navbar">
-          <ul className="navbar-list">
-            <li className="navbar-list-item"><a className="nav-link" href="index.html">About Me</a></li>
-            <li className="navbar-list-item"><a className="nav-link" href="blog/blog-index.html">Foundations Blog</a></li>
-            <li className="navbar-list-item"><a className="nav-link" href="https://megan-duncan.github.io/minesweeper/">PipiSurfer</a></li>
-            <li className="navbar-list-item"><a className="nav-link" href="https://funpage3.herokuapp.com/#/">Fun Page</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div >
+    <div className="container">
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-list-item" onClick={() => handleClick('home')}><p> Home </p></li>
+          <li className="navbar-list-item" onClick={() => handleClick('blog-index')}><p> Foundations Blog </p></li>
+          <li className="navbar-list-item"><a className="nav-link" href="https://megan-duncan.github.io/minesweeper/">PipiSurfer</a></li>
+          <li className="navbar-list-item"><a className="nav-link" href="https://funpage3.herokuapp.com/#/">Fun Page</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div >
    )
 };
 
